0d470148027f729e842470a6e19b12ddeab50b60
PURBITAM <66543712+PURBITAM@users.noreply.github.com>
2021-05-12 20:20:17 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+filenum=$(ls | wc -l)
+
+function guess_num {
+	read guess
+	if [[ $guess -eq $filenum ]]
+	then
+		echo "Great!!! You finally guessed it!"
+		echo "Congratulation"
+		echo "  So we have, "
+		for f in $(ls)
+		do
+			echo "  - $f and "
+		done
+		echo "    and that was it."
+	else
+		if [[ $guess -gt $filenum ]]
+		then
+			echo "There is less, try again and press Enter :"
+			guess_num
+		else
+			echo "There is more, try again and press Enter :"
+			guess_num
+		fi
+	fi
+}
+echo "Welcome to the guessing game!"
+echo "Guess how many files are in the current directory and press Enter :"
+guess_num
---
+++ makefile
@@ -0,0 +1,7 @@
+readme : guessinggame.sh
+	touch README.md
+	echo "TITLE OF THE PROJECT : Peer-graded Assignment - Guessing Game" > README.md
+	echo "\nDate make ran at:" >> README.md
+	date >> README.md
+	echo "\nThe Number of lines present in guessinggame.sh : " >> README.md
+	cat guessinggame.sh | wc -l >> README.md
